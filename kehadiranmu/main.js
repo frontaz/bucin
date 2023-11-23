58cc5a5253f8d7c73ca28314fbe4d329b5b71996
frontaz <58595301+frontaz@users.noreply.github.com>
2023-11-23 14:51:56 +0700

    Add files via upload

---
+++ kehadiranmu/main.js
@@ -0,0 +1,15 @@
+const audio = document.getElementById("audio");
+audio.addEventListener("play", handleFirstPlay, false);
+
+let hasPlayed = false;
+function handleFirstPlay(event) {
+  if (!hasPlayed) {
+    hasPlayed = true;
+
+    // Remove listener so this only gets called once.
+    const vid = event.target;
+    vid.removeEventListener("play", handleFirstPlay);
+
+    // Start whatever you need to do after first playback has started
+  }
+}
